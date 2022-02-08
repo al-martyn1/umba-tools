803fba7969decec835e7fc523c28063aaf11059e
Alexander Martynov <amart@mail.ru>
2022-02-08 22:50:01 +0300

    Custom options file changed to .example in distr

--- make_distr.bat
+++ make_distr.bat
@@ -73,9 +73,8 @@ goto END
 @set ZIP_ROOT=%DISTR_ROOT%\%PLATFORM%\%CONFIGURATION%
 
 @mkdir %TARGET_ROOT%\bin
-@xcopy /Y /S /E /I /F /R ..\umba-brief-scanner\_distr_conf\conf\*    %TARGET_ROOT%\conf
-@rem xcopy /Y /S /E /I /F /R ..\umba-pretty-headers\_distr_conf\conf\umba-pretty-headers.options   %TARGET_ROOT%\conf\umba-pretty-headers.options
-@copy /Y                 ..\umba-pretty-headers\_distr_conf\conf\umba-pretty-headers.options       %TARGET_ROOT%\conf\umba-pretty-headers.options
+@xcopy /Y /S /E /I /F /R     ..\umba-brief-scanner\_distr_conf\conf\*                                             %TARGET_ROOT%\conf
+@copy /Y                     ..\umba-pretty-headers\_distr_conf\conf\umba-pretty-headers.custom.options           %TARGET_ROOT%\conf\umba-pretty-headers.custom.options.example
 
 @copy %BUILD_OUTPUT%\%MAIN_EXE_NAME%.exe        %TARGET_ROOT%\bin\%MAIN_EXE_NAME%.exe
 @copy %BUILD_OUTPUT%\umba-make-headers.exe      %TARGET_ROOT%\bin\umba-make-headers.exe
