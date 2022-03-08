89ae27158b16072bc0c4d380b2e50475ed9d1d50
Alexander Martynov <amart@mail.ru>
2022-03-08 20:25:29 +0300

    Added clone script

---
+++ clone-tools.bat
@@ -0,0 +1,6 @@
+cd ..
+if not exist umba-brief-scanner  git clone git@github.com:al-martyn1/umba-brief-scanner.git
+if not exist umba-make-headers   git clone git@github.com:al-martyn1/umba-make-headers.git
+if not exist umba-pretty-headers git clone git@github.com:al-martyn1/umba-pretty-headers.git
+if not exist umba-subst-macros   git clone git@github.com:al-martyn1/umba-subst-macros.git
+cd umba-tools
